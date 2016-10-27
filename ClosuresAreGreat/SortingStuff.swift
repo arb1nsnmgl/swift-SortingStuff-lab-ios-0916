88f2542debc647bf1298329ed1345ab50e101931
arb1nsnmgl <arvinsanmiguel@gmail.com>
2016-10-27 14:25:39 -0400

    Completed

--- ClosuresAreGreat/SortingStuff.swift
+++ ClosuresAreGreat/SortingStuff.swift
@@ -18,6 +18,45 @@ struct ToyBin {
     
     // TODO: Implement all of the sort functions (lets organize this toy bin!)
     
+    mutating func sortShips() -> [Ship] {
+        
+        ships = ships.sorted(by: {$0.age > $1.age})
+        for (index, ship) in ships.enumerated() {
+            if ship.name == "Titanic" {
+                let titanic = ships.remove(at: index)
+                ships.insert(titanic, at: 0)
+            }
+        }
+        return ships
+    }
+    
+    mutating func sortBooks() -> [Book] {
+        return books.sorted(by: { $0.name < $1.name })
+    }
+    
+    mutating func sortBowlingPins() -> [BowlingPin] {
+        return bowlingPins.sorted(by: { $0.color.rawValue < $1.color.rawValue })
+    }
+    
+    mutating func sortMusicCDs() -> [MusicCD] {
+        
+        musicCDs = musicCDs.sorted(by: {$0.name > $1.name })
+        for (index, music) in musicCDs.enumerated() {
+            if music.name == "Drake" {
+                let drake = musicCDs.remove(at: index)
+                musicCDs.insert(drake, at: 0)
+            }
+        }
+        return musicCDs
+    }
+    
+    mutating func changeColorOfAllPins(to color: Color) -> [BowlingPin] {
+        return bowlingPins.map({ pin in
+            var newPin = pin
+            newPin.changeColor(to: color)
+            return newPin
+        })
+    }
  
 }
 
@@ -39,6 +78,11 @@ struct BowlingPin {
     var name: String
     var film: String
     var color: Color
+    
+    mutating func changeColor(to color: Color) {
+        self.color = color
+    }
+    
 }
 
 
